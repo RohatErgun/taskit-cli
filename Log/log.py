fd6f555086f171efde0388ae3506c6c199cfb50a
RohatErgun <rohatergun06@gmail.com>
2025-11-19 12:16:57 +0100

    Added log feature

---
+++ Log/log.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+
+"""
+    adding logs of user's commands which functions used and what is the outcome
+    keeps the record in the txt file
+"""
+
+
+class LogSystem:
+    @staticmethod
+    def logged(function):
+
+        def wrapper(*args, **kwargs):
+            now = datetime.now()
+            values = function(*args, **kwargs)
+
+            with open('logfile.tx', 'a+') as f:
+                fun_name = function.__name__
+                timestamp = now.day, now.month, now.year
+                f.write(f"\nUser used: <{fun_name}> function has returned with:\t{values}\ttimestamp:{timestamp}")
+
+            return values
+        return wrapper
+
